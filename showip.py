d8e0f2b702c5569667b9560bab5b510170b932b2
Kent Daleng <kentsd16@student.uia.no>
2019-01-15 10:31:48 +0000

    Try to establish a network connection until it succeeds

--- showip.py
+++ showip.py
@@ -5,8 +5,8 @@ import socket
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("1.1.1.1", 80))
     try:
-        s.connect("192.255.255.255", 1)
         ip = s.getsockname()[0]
     except:
         ip = "127.0.0.1"
@@ -19,3 +19,11 @@ def main():
     ip = get_local_ip()
     for _ in range(10):
         sense.show_message(ip, scroll_speed=0.2)
+
+if __name__ == '__main__':
+    while True:
+         try:
+             main()
+             break
+         except OSError:
+             continue
